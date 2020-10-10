863f62b55b94fd28e8edfc35a667dd3a03a7750d
Asif <klmktu.asif@gmail.com>
2020-10-10 18:55:20 +0530

    tt

--- index.js
+++ index.js
@@ -80,8 +80,16 @@ client.on("message", async function(message) {
     let d = new Date();
     let nextDay = 0;
     if (d.getHours() > 17) {
-      if (d.getDay() >= 5) nextDay = d.getDay() % 5 + 3;
+      if (d.getDay() === 6) nextDay = 2;
+      else if (d.getDay() >= 5) nextDay = d.getDay() % 5 + 3;
       else nextDay = 1;
+    }
+    else {
+      if (d.getDay() === 6) {
+        nextDay = 2;
+      } else if (d.getDay() === 0) {
+        nextDay = 1;
+      }
     }
     getTimeTable(nextDay, (day, data) => {
       embedMessage(message.channel, day, data);
